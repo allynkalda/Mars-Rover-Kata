deecb240567b7eefdba627bd3442427e94493cc8
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-24 20:40:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var rover = {
-	direction: "N",
-	x: 0,
-	y: 0,
-	travelog: [],
+  direction: "N",
+  x: 0,
+  y: 0,
+  travelog: [],
 }
 
 var roverDir = rover.direction;
@@ -25,10 +25,13 @@ var board = [
 ];
 // Input command:
 
-commands("rfbfffff");
+commands("rff");
 
 console.log(rover.travelog);
+console.log(roverDir);
+console.log(board[roverY][roverX]);
 
+// Run loop over command
 
 function commands(stuff) {
 for (var i = 0; i < stuff.length; i++) {
@@ -37,75 +40,65 @@ var letter = stuff.charAt(i);
 
 
 switch (letter) {
-	case "f":
-	checkGrid();
-	break;
-	case "r":
-	turnRight();
-	break;
-	case "l":
-	turnLeft();
-	case "b":
-	checkGrid();
-	break;
-	default:
-	return;
-			}
-		}
+  case "f":
+  moveForward();
+  break;
+  case "r":
+  turnRight();
+  break;
+  case "l":
+  turnLeft();
+  case "b":
+  moveBackward();
+  break;
+  default:
+  return;
+      }
+    }
 
-function obstacle() {
+/*   } else if (letter === "f") {
+    rover.travelog.push(moveForward());
+  } else if (letter === "b") {
+    rover.travelog.push(moveBackward());
+  }
 
-
-	if (board[roverX][roverY] === "S") {
-		console.log("Obstacle found at X: " + roverX + " and Y: " + roverY);
-	} else if (letter === "f") {
-		rover.travelog.push(moveForward());
-	} else if (letter === "b") {
-		rover.travelog.push(moveBackward());
-	}
-}
-
-/*
-	if (letter === "f") {
-		obstacle();
-		rover.travelog.push(moveForward());
-	} else if (letter === "b") {
-		obstacle();
-		rover.travelog.push(moveBackward());
-		}
-	}
 */
-	// alerts when rover X or Y runs off grid
+
+  // alerts when rover X or Y runs off grid
 
 function checkGrid() {
-	if (roverX > 9) {
-		alert("Rover has run off grid!");
-		return;
-	} else if (roverY > 9) {
-		alert("Rover has run off grid!");
-		return;
-	} else {
-		obstacle();
-	}
-}
+  if (roverX > 9) {
+    alert("Rover has run off grid!");
+    return;
+  } else if (roverY > 9) {
+    alert("Rover has run off grid!");
+    return;
+  } else if (roverX < 0) {
+    alert("Rover has run off grid!");
+    roverX++;
+  } else if (roverY < 0) {
+    alert("Rover has run off grid!");
+    roverX++;
+  }
 
 
-}		
+
+}   
 
 
 function turnRight() {
 switch (roverDir) {
-	case "N":
-	roverDir = "E";
-	break;
-	case "E":
-	roverDir = "S";
-	break;
-	case "S":
-	roverDir = "W";
-	break;
-	case "W":
-	roverDir = "N";
+  case "N":
+  roverDir = "E";
+  break;
+  case "E":
+  roverDir = "S";
+  break;
+  case "S":
+  roverDir = "W";
+  break;
+  case "W":
+  roverDir = "N";
 }
 return roverDir;
 }
@@ -113,17 +106,17 @@ return roverDir;
 
 function turnLeft() {
 switch (roverDir) {
-	case "N":
-	roverDir = "W";
-	break;
-	case "W":
-	roverDir = "S";
-	break;
-	case "S":
-	roverDir = "E";
-	break;
-	case "E":
-	roverDir = "N";
+  case "N":
+  roverDir = "W";
+  break;
+  case "W":
+  roverDir = "S";
+  break;
+  case "S":
+  roverDir = "E";
+  break;
+  case "E":
+  roverDir = "N";
 }
 return roverDir;
 }
@@ -131,40 +124,49 @@ return roverDir;
 function moveForward() {
 
 switch (roverDir) {
-	case "N":
-	roverY -= 1;
-	break;
-	case "S": 
-	roverY += 1;
-	break;
-	case "W": 
-	roverX -= 1;
-	break;
-	case "E": 
-	roverX += 1;
+  case "N":
+  roverY -= 1;
+  break;
+  case "S": 
+  roverY += 1;
+  break;
+  case "W": 
+  roverX -= 1;
+  break;
+  case "E": 
+  roverX += 1;
 
 }
 
-return "X: " + roverX + ", Y: " + roverY;
+checkGrid();
+  if (board[roverY][roverX] === "S") {
+    console.log("Obstacle found at X: " + roverX + " and Y: " + roverY +"!");
+    moveBackward();
+    
+    }
+
+  rover.travelog.push("X: " + roverX + ", Y: " + roverY);
 }
 
 
 function moveBackward() {
 
 switch (roverDir) {
-	case "N":
-	roverY += 1;
-	break;
-	case "S": 
-	roverY -= 1;
-	break;
-	case "W": 
-	roverX += 1;
-	break;
-	case "E": 
-	roverX -= 1;
+  case "N":
+  roverY += 1;
+  break;
+  case "S": 
+  roverY -= 1;
+  break;
+  case "W": 
+  roverX += 1;
+  break;
+  case "E": 
+  roverX -= 1;
 
 }
 
-return "X: " + roverX + ", Y: " + roverY;
+console.log("X: " + roverX + ", Y: " + roverY);
+}
+
 }
